4ef15f3ec6befa876eb198160119495b40472287
JONGHYUNVAN <116232652+JONGHYUNVAN@users.noreply.github.com>
2024-01-06 10:08:50 +0900

    feat: 로그아웃시 refreshtoken 삭제
    로그아웃시 refreshtoken 삭제 기능 구현

--- src/main/java/com/demo/library/security/repository/RefreshTokenRepository.java
+++ src/main/java/com/demo/library/security/repository/RefreshTokenRepository.java
@@ -2,6 +2,7 @@ package com.demo.library.security.repository;
 
 
 import com.demo.library.security.entity.RefreshToken;
+import com.demo.library.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
@@ -12,4 +13,6 @@ public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long
     Optional<RefreshToken> findByToken(String token);
     void deleteByToken(String token);
     void deleteAllByExpiryDateTimeBefore(Date now);
+
+    void deleteByUser(User user);
 }
