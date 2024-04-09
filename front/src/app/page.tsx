f1e34220ca40ea7c9179dafa3083c6726f5ea227
JONGHYUNVAN <116232652+JONGHYUNVAN@users.noreply.github.com>
2024-04-09 21:01:19 +0900

    refactor: main 메인 페이지 애니메이션 효과 추가

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -10,12 +10,27 @@ export default function Home() {
           </head>
 
           <div className="main-page">
-            <div className="title" style={{ position: 'relative', backgroundSize: 'cover', backgroundPosition: 'center' }}>
-              <h4>library project</h4>
-              <span className="title-sub">by {name}</span>
-            </div>
+              <div className="title"
+                   style={{position: 'relative', backgroundSize: 'cover', backgroundPosition: 'center'}}>
+                  <h4 className="titleLetter">L</h4>
+                  <h4 className="titleLetter">i</h4>
+                  <h4 className="titleLetter">b</h4>
+                  <h4 className="titleLetter">r</h4>
+                  <h4 className="titleLetter">a</h4>
+                  <h4 className="titleLetter">r</h4>
+                  <h4 className="titleLetter">y</h4>
+                  <span className="title-space"></span>
+                  <h4 className="titleLetter">p</h4>
+                  <h4 className="titleLetter">r</h4>
+                  <h4 className="titleLetter">o</h4>
+                  <h4 className="titleLetter">j</h4>
+                  <h4 className="titleLetter">e</h4>
+                  <h4 className="titleLetter">c</h4>
+                  <h4 className="titleLetter">t</h4>
+                  <span className="title-sub">by {name}</span>
+              </div>
               <Link href="/ranking">
-                <video poster={"/backposter.png"} className="background-video" autoPlay muted loop >
+              <video poster={"/backposter.png"} className="background-video" autoPlay muted loop >
                   <source src="/background.mp4" type="video/mp4" />
                 </video>
               </Link>
