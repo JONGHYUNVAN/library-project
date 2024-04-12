8466a184a903b2291e0009f8a37d4b7563c4379c
JONGHYUNVAN <116232652+JONGHYUNVAN@users.noreply.github.com>
2024-04-12 22:25:09 +0900

    feat:post post crud 구현

--- front/src/app/cursor/page.tsx
+++ front/src/app/cursor/page.tsx
@@ -8,18 +8,18 @@ export default function Cursor() {
     useEffect(() => {
         const pc = particlesCursor({
             el: document.getElementById('app'),
-            gpgpuSize: 1000,// 갯수
+            gpgpuSize: 250,// 갯수
             color: 0xDAA520,// 입자색
             colors: [0xDAA520, 0x800000],//불꽃 색 범위
-            coordScale: 0.7,//운동계수
-            pointSize: 15,//크기
-            noiseIntensity: 0.003,//노이즈 강도
-            noiseTimeCoef: 0.01,//노이즈 변화율
-            pointDecay: 0.005,//소멸속도
-            sleepRadiusX: 25, // 커서와의 거리들
-            sleepRadiusY: 25,
-            sleepTimeCoefX: 0.001,// 움직일때 비활성
-            sleepTimeCoefY: 0.002,
+            coordScale: 1,//운동계수
+            pointSize: 12,//크기
+            noiseIntensity: 0.001,//노이즈 강도
+            noiseTimeCoef: 0.0001,//노이즈 변화율
+            pointDecay: 0.002,//소멸속도
+            sleepRadiusX: 500, // 커서와의 거리들
+            sleepRadiusY: 500,
+            sleepTimeCoefX: 0.01,// 움직일때 비활성
+            sleepTimeCoefY: 0.01,
         });
         const handleClick = () => {
             pc.uniforms.uColor.value.set(Math.random() * 0xffffff);
